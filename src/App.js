78188dfb29f1e170bbf39e0e62e00129987ff2bb
Aman kukna <aman.kukna.007@gmail.com>
2024-05-11 18:31:54 +0530

    house rent
    all pages going to work

--- src/App.js
+++ src/App.js
@@ -8,11 +8,6 @@ import Photog from './rent/photog.js';
 import Rent from './rent/rent';
 import About from './rent/aboutus';
 import Form from './com/form'
-import Admin from './Admin/admin'
-import Addcat from './Admin/addcat';
-import Viewcat from './Admin/viewcat';
-import Addtable from './Admin/addtable';
-import Viewtable from './Admin/viewtable';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -28,11 +23,6 @@ function App() {
         <Route path="/rent" element={<Rent/>} />
         <Route path="/about-us" element={<About/>} />
         <Route path='/form' element={<Form/>}/>
-        <Route path='/form/admin' element={<Admin/>}/>
-        <Route path="/form/admin/add-catagories" element={<Addcat/>} />
-        <Route path="/form/admin/view-catagories" element={<Viewcat/>} />
-        <Route path="/form/admin/add-table" element={<Addtable/>} />
-        <Route path="/form/admin/view-table" element={<Viewtable/>} />
         
         </Routes>
     </BrowserRouter>
